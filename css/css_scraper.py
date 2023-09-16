ed1e606e17539a515191b53f01f9dd6402d95f12
Nestor Reategui <nestor_remo@csu.fullerton.edu>
2023-09-15 18:30:56 -0700

    sidebar scroll?

---
+++ css/css_scraper.py
@@ -0,0 +1,8 @@
+file1 = open('index.css', 'r')
+lines = file1.readlines()
+setA = set()
+for line in lines:
+    if ";" in line:
+        setA.add((line.split(':')[0]).strip())
+print(setA)
+print(len(setA))
